bf5488df672d3525adb647b2d3e4ae2820d4d6bc
Christian Luu <35637788+spacemen13579@users.noreply.github.com>
2019-11-20 15:06:25 -0500

    Revert "movement"
    This reverts commit 6d4601fb31eda085372f6c1d4d76215ac43118ae.

--- Main/main2.py
+++ Main/main2.py
@@ -11,12 +11,11 @@ class BlockObject(pygame.sprite.Sprite):
         self.gameX = gameCoords[0]  # int
         self.gameY = gameCoords[1]
         self.gameZ = gameCoords[2]
-        self.direction = "y" # facing down to bottom left at first
 
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, gameCoords=(100, 100, 100)):
-        super().__init__()
+        super.__init()
         self.gameX = gameCoords[0]  # integer
         self.gameY = gameCoords[1]
         self.gameZ = gameCoords[2]
@@ -24,31 +23,6 @@ class Player(pygame.sprite.Sprite):
         # self.surfDownRight = pygame.image.load("playerdownright.png")
         # self.surfUpLeft = pygame.image.load("playerupleft.png")
         # self.surfUpRight = pygame.image.load("playerupright.png")
-    
-    def move(self, dx, dy, dz, locationMap):
-        self.gameX += dx
-        self.gameY += dy
-        self.gameZ += dz
-        # keep track of where the player is facing (helpful for placing and
-        # destroying blocks) Also useful for movement also
-        if (dy > 0):
-            self.direction = "y"
-        elif (dy < 0):
-            self.direction = "-y"
-        elif (dx > 0):
-            self.direction = "x"
-        elif (dx < 0):
-            self.direction = "-x"
-        # direction dz doesn't matter
-        print("player pos: ", self.gameX, self.gameY, self.gameZ)
-        # should implement isLegalMove into move here...
-        # probably should do bounds checking here also
-    
-    def getPos(self):
-        return (self.gameX, self.gameY, self.gameZ)
-    
-    def placeBlock(self, block, locationMap):
-        pass  #place down block in front of player
 
 
 class Minecraft(PygameGame):
@@ -71,7 +45,6 @@ class Minecraft(PygameGame):
             xSize=self.gameDims[0],
             ySize=self.gameDims[1],
             zSize=self.gameDims[2])
-        self.player = Player((self.gameDims[0]//2, self.gameDims[1]//2, self.gameDims[2]//2))
 
     @staticmethod
     def initBlockTexture():
@@ -153,39 +126,22 @@ class Minecraft(PygameGame):
                     newBlock = BlockObject("sand", (x, y, z))
                     self.locationMap[x, y, z] = newBlock
                     self.gameBlockGroup.add(newBlock)
-        
-        # for debugging
-        for x in range(xSize//2 - 2, xSize//2 + 3, 2):
-            for y in range(ySize//2 - 2, xSize//2 + 3, 2):
-                self.gameBlockGroup.remove(BlockObject("grassDirt"))
-                centerBlock = BlockObject("stone", (x, y, 100))  #init at center of map
-                self.locationMap[x, y, 100] = centerBlock
-                self.gameBlockGroup.add(centerBlock)
 
-
-    def drawWorld(self, screen, playerPos):
+    def drawWorld(self, screen, posX, posY, posZ):
         # posX, posY, posZ is center of drawing; current location of player
         # for loops chosen CAREFULLY to be able to blit surfaces properly...
-        self.screen.blit(self.background, (0, 0))
-        posX = playerPos[0]
-        posY = playerPos[1]
-        posZ = playerPos[2]
+        curr = time.time()
         for x in range(posX - self.renderDist, posX + self.renderDist):
             for y in range(posY - self.renderDist, posY + self.renderDist):
                 for z in range(posZ - self.renderDist, posZ + self.renderDist):
-                    self.drawBlock(screen, self.locationMap[x, y, z], (x, y, z),
-                        playerPos)
+                    self.drawBlock(screen, self.locationMap[x, y, z], x, y, z,
+                        posX, posY, posZ)
+        print(time.time() - curr)
 
 # Very heavily adapted from http://clintbellanger.net/articles/isometric_math/
-    def drawBlockColors(self, screen, blockObj, centerPos, playerPos):
-        posX = playerPos[0]
-        posY = playerPos[1]
-        posZ = playerPos[2]
-        x = centerPos[0]
-        y = centerPos[1]
-        z = centerPos[2]
+    def drawBlockColors(self, screen, blockObj, x, y, z, posX, posY, posZ):
         # right corner top surface
-        x0 = ((x - posX) - (y - posY))*self.blockXWidth/2 + self.width/2 + self.blockXWidth/2
+        x0 = ((x - posX) - (y - posY))*self.blockXWidth/2 + self.width/2 + self.margin
         y0 = ((x - posX) + (y - posY))*self.blockXWidth/(2*2) + self.height/2 - (z - posZ)*self.blockXWidth/2
         # bottom corner top surface
         x1 = x0 - self.blockXWidth/2
@@ -231,17 +187,11 @@ class Minecraft(PygameGame):
         posRightSurface = [(x0Right, y0Right), (x1Right, y1Right), (x2Right, y2Right), (x3Right, y3Right)]
         pygame.draw.polygon(screen, (255, 0, 0), posRightSurface)
 
-    def drawBlock(self, screen, block, centerPos, playerPos):
-        posX = playerPos[0]
-        posY = playerPos[1]
-        posZ = playerPos[2]
-        x = centerPos[0]
-        y = centerPos[1]
-        z = centerPos[2]
+    def drawBlock(self, screen, block, x, y, z, posX, posY, posZ):
         if (block.name == "empty"):
             return 0
         blockSurface = Minecraft.blockLib[block.name]
-        drawX = ((x - posX) - (y - posY))*self.blockXWidth/2 + self.width/2 + self.blockXWidth/2 - self.blockXWidth  #new test
+        drawX = ((x - posX) - (y - posY))*self.blockXWidth/2 + self.width/2 + self.margin - self.blockXWidth
         drawY = ((x - posX) + (y - posY))*self.blockXWidth/(2*2) + self.height/2 - self.blockXWidth/(2*2) - (z - posZ)*self.blockXWidth/2
         self.screen.blit(blockSurface, (drawX, drawY))
         pygame.draw.line(screen, (255, 255, 255), (self.width/2, 0), (self.width/2, self.height))
@@ -260,19 +210,12 @@ class Minecraft(PygameGame):
         pass
 
     def keyPressed(self, keyCode, modifier):
-        if (keyCode == pygame.K_UP):
-            self.player.move(-1, 0, 0, self.locationMap)
-        elif (keyCode == pygame.K_DOWN):
-            self.player.move(1, 0, 0, self.locationMap)
-        elif (keyCode == pygame.K_RIGHT):
-            self.player.move(0, -1, 0, self.locationMap)
-        elif (keyCode == pygame.K_LEFT):
-            self.player.move(0, 1, 0, self.locationMap)
-        elif (keyCode == pygame.K_w):  # move up into sky
-            self.player.move(0, 0, 1, self.locationMap)
-        elif (keyCode == pygame.K_s):  # move down into ground
-            self.player.move(0, 0, -1, self.locationMap)
-        
+        # self.drawBlock(self.screen, "HEELLO", 99, 99, 100, 100, 100, 100)
+        #self.drawBlock(self.screen, "HEELLO", 100, 100, 100, 100, 100, 100)
+        # self.drawBlock(self.screen, "HEELLO", 101, 101, 100, 100, 100, 100)
+        # self.drawBlock(self.screen, "HEELLO", 102, 102, 100, 100, 100, 100)
+        # self.drawBlock(self.screen, "HEELLO", 101, 100, 100, 100, 100, 100)
+        self.drawWorld(self.screen, 100, 100, 100)
 
     def keyReleased(self, keyCode, modifier):
         pass
@@ -281,7 +224,7 @@ class Minecraft(PygameGame):
         pass
 
     def redrawAll(self, screen):
-        self.drawWorld(screen, self.player.getPos())
+        pass
     
 game = Minecraft()
         
