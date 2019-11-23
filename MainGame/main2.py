91711fdb973d669b2536b9295be94f2dac6b5514
Christian Luu <35637788+spacemen13579@users.noreply.github.com>
2019-11-23 00:30:21 -0500

    edited stuff
    remember stuff about random map generation and gaussian blur

--- MainGame/main2.py
+++ MainGame/main2.py
@@ -29,25 +29,25 @@ class Player(pygame.sprite.Sprite):
         # self.surfUpRight = pygame.image.load("playerupright.png")
     
     def move(self, dx, dy, dz, locationMap):
-        if (self.getLegalPlayerPos(dx, dy, dz, locationMap) == None):
-            print("Cannot move to:", (self.gameX + dx, self.gameY + dy, self.gameZ + dz))
+        if (self.getLegalPlayerPos(dx, dy, dz, locationMap) == False):
+            print("Cannot move to:", (self.gameX + dx, self.gameY + dy, self.gameZ + dz), "Current Pos:", (self.gameX, self.gameY, self.gameZ))
             return False # cannot move
         else:
             (x, y, z) = self.getLegalPlayerPos(dx, dy, dz, locationMap)
-            
-        # self.gameX += dx
-        # self.gameY += dy
-        # self.gameZ += dz
-        # # keep track of where the player is facing (helpful for placing and
-        # # destroying blocks) Also useful for movement also
-        # if (dy > 0):
-        #     self.direction = "+y"
-        # elif (dy < 0):
-        #     self.direction = "-y"
-        # elif (dx > 0):
-        #     self.direction = "+x"
-        # elif (dx < 0):
-        #     self.direction = "-x"
+            self.gameX = x
+            self.gameY = y
+            self.gameZ = z
+            # keep track of where the player is facing (helpful for placing and
+            # destroying blocks) Also useful for movement also
+            if (dy > 0):
+                self.direction = "+y"
+            elif (dy < 0):
+                self.direction = "-y"
+            elif (dx > 0):
+                self.direction = "+x"
+            elif (dx < 0):
+                self.direction = "-x"
+            print("Moved to:", (x, y, z), "/ Dir:", self.direction)
         # # direction dz doesn't matter
         # print("Player pos:", self.gameX, self.gameY, self.gameZ, "/ Dir:", self.direction)
         # # should implement isLegalMove into move here...
@@ -57,7 +57,7 @@ class Player(pygame.sprite.Sprite):
         return (self.gameX, self.gameY, self.gameZ)
     
     def placeBlock(self, block, locationMap, gameBlockGroup):
-        if (self.getPlaceBlockLocation(locationMap) == None):
+        if (self.getPlaceBlockLocation(locationMap) == False):
             print("Block not placed")
             return False # block not placed
         else:
@@ -69,43 +69,43 @@ class Player(pygame.sprite.Sprite):
     
     def getPlaceBlockLocation(self, locationMap):  # gets location at which block has to be placed
         if (self.direction == "+y"): # facing down the y direction
-            (x, y, z) = (self.gameX, self.gameY + 1, self.gameZ + 1)  # coords being considered; add 1 to gameZ beause
+            (x, y, z) = (self.gameX, self.gameY + 1, self.gameZ)  # coords being considered; block to be placed is at same level as player
             # player pos is inside the block itself so we need to shift up
             if (locationMap[x, y, z].name == "empty" and locationMap[x, y, z - 1].name != "empty"): # check legal block placement
                 return (x, y, z)
-            else: return None # none is when cannot place block
+            else: return False # false is when cannot place block
         elif (self.direction == "-y"): # facing opposite y direction
-            (x, y, z) = (self.gameX, self.gameY - 1, self.gameZ + 1)
+            (x, y, z) = (self.gameX, self.gameY - 1, self.gameZ)
             if (locationMap[x, y, z].name == "empty" and locationMap[x, y, z - 1].name != "empty"):
                 return (x, y, z)
-            else: return None
+            else: return False
         elif (self.direction == "+x"): # facing down the x direction
-            (x, y, z) = (self.gameX + 1, self.gameY, self.gameZ + 1)
+            (x, y, z) = (self.gameX + 1, self.gameY, self.gameZ)
             if (locationMap[x, y, z].name == "empty" and locationMap[x, y, z - 1].name != "empty"):
                 return (x, y, z)
-            else: return None
+            else: return False
         elif (self.direction == "-y"): # facing opposite z direction
-            (x, y, z) = (self.gameX, self.gameY - 1, self.gameZ + 1)
+            (x, y, z) = (self.gameX, self.gameY - 1, self.gameZ)
             if (locationMap[x, y, z].name == "empty" and locationMap[x, y, z - 1].name != "empty"):
                 return (x, y, z)
-            else: return None
+            else: return False
         else:
             raise Exception("Block location/name not found")
     
     def getLegalPlayerPos(self, dx, dy, dz, locationMap):  # gets location at which block has to be placed
         if (dx != 0): # move in x
             (x, y, z) = (self.gameX + dx, self.gameY, self.gameZ) # that player about to move to
-            if (locationMap[x, y, z].name == "empty" and locationMap[x, y, z + 1] == "empty"): # player takes up two z-blocks
+            if (locationMap[x, y, z].name == "empty" and locationMap[x, y, z + 1].name == "empty"): # player takes up two z-blocks
                 return (x, y, z)
             else: return False
         elif (dy != 0): # move in y
             (x, y, z) = (self.gameX, self.gameY + dy, self.gameZ) # that player about to move to
-            if (locationMap[x, y, z].name == "empty" and locationMap[x, y, z + 1] == "empty"): # player takes up two z-blocks
+            if (locationMap[x, y, z].name == "empty" and locationMap[x, y, z + 1].name == "empty"): # player takes up two z-blocks
                 return (x, y, z)
             else: return False
         elif (dz != 0): # move in z
             (x, y, z) = (self.gameX, self.gameY , self.gameZ + dz) # that player about to move to
-            if (locationMap[x, y, z].name == "empty" and locationMap[x, y, z + 1] == "empty"): # player takes up two z-blocks
+            if (locationMap[x, y, z].name == "empty" and locationMap[x, y, z + 1].name == "empty"): # player takes up two z-blocks
                 return (x, y, z)
             else: return False
 
@@ -127,36 +127,12 @@ class Minecraft(PygameGame):
         self.screen.blit(self.background, (0, 0))
         self.gameBlockGroup = pygame.sprite.Group()
         self.initBlockLibrary()  # group textures
+        self.perspective = 1 # default perspective, with origin up top
         self.createFlatWorld(
             xSize=self.gameDims[0],
             ySize=self.gameDims[1],
             zSize=self.gameDims[2])
         self.player = Player((self.gameDims[0]//2, self.gameDims[1]//2, self.gameDims[2]//2 + 1)) # player pos is above the block player is standing on
-
-    @staticmethod
-    def initBlockTexture():
-        # DIRT, GRASSDIRT, COBBLESTONE, WOOD, IRON
-        # sid = side (short name)
-        Minecraft.blockTex = {
-        # textures from https://github.com/supernick125/esap-final/tree/master/textures
-            "dirtTop": pygame.image.load("textures/dirt.png").convert(),
-            "dirtSidL": pygame.image.load("textures/dirt.png").convert(),
-            "dirtSidR": pygame.image.load("textures/dirt.png").convert(),
-            "stonTop": pygame.image.load("textures/stone.png").convert(),
-            "stonSidL": pygame.image.load("textures/stone.png").convert(),
-            "stonSidR": pygame.image.load("textures/stone.png").convert(),
-            "bricTop": pygame.image.load("textures/brick.png").convert(),
-            "bricSidL": pygame.image.load("textures/brick.png").convert(),
-            "bricSidR": pygame.image.load("textures/brick.png").convert(),
-            "pumpTop":
-                pygame.image.load("textures/pumpkin_top.png").convert(),
-            "pumpSidL":
-                pygame.image.load("textures/pumpkin_side.png").convert(),
-            "pumpSidR":
-                pygame.image.load("textures/pumpkin_side.png").convert(),
-            "grasTop": pygame.image.load("textures/grass_top.png").convert(),
-            "grasSidL": pygame.image.load("textures/grass_side.png").convert(),
-            "grasSidR": pygame.image.load("textures/grass_side.png").convert()}
     
     def initBlockLibrary(self):
         Minecraft.blockLib = {
@@ -224,16 +200,23 @@ class Minecraft(PygameGame):
                 self.locationMap[x, y, 100] = centerBlock
                 self.gameBlockGroup.add(centerBlock)
 
-    def drawWorld(self, screen, playerPos):
+    def drawWorld(self, screen, playerPos, perspective): # perspective is either 1 (default), 2, 3, 4 (rotating map from bird-eye view clockwise)
         self.screen.blit(self.background, (0, 0))
         posX = playerPos[0]
         posY = playerPos[1]
         posZ = playerPos[2] - 1  # player pos is above the block player is standing on so need to -1 to draw
-        for x in range(posX - self.renderDist, posX + self.renderDist):
-            for y in range(posY - self.renderDist, posY + self.renderDist):
-                for z in range(posZ - self.renderDist, posZ + self.renderDist):
-                    self.drawBlock(screen, self.locationMap[x, y, z], (x, y, z),
-                        playerPos)
+        if (perspective == 1):
+            for x in range(posX - self.renderDist, posX + self.renderDist):
+                for y in range(posY - self.renderDist, posY + self.renderDist):
+                    for z in range(posZ - self.renderDist, posZ + self.renderDist):
+                        self.drawBlock(screen, self.locationMap[x, y, z], (x, y, z),
+                            playerPos)
+        elif (perspective == 2):
+            for x in range(posX - self.renderDist, posX + self.renderDist):
+                for y in range(posY + self.renderDist - 1, posY - self.renderDist - 1, -1):
+                    for z in range(posZ - self.renderDist, posZ + self.renderDist):
+                        self.drawBlock(screen, self.locationMap[x, y, z], (x, y, z),
+                            playerPos)
 
 # Isometric drawing VERY HEAVILY adapted from http://clintbellanger.net/articles/isometric_math/
     def drawBlockColors(self, screen, blockObj, centerPos, playerPos):
@@ -301,7 +284,7 @@ class Minecraft(PygameGame):
             return 0
         blockSurface = Minecraft.blockLib[block.name]
         #drawX = ((x - posX) - (y - posY))*Minecraft.blockXWidth/2 + self.width/2 + self.margin - Minecraft.blockXWidth
-        drawX = ((x - posX) - (y - posY))*Minecraft.blockXWidth/2 + self.width/2 + Minecraft.blockXWidth/2 - Minecraft.blockXWidth  #new test
+        drawX = ((x - posX) - (y - posY))*Minecraft.blockXWidth/2 + self.width/2 + Minecraft.blockXWidth/2 - Minecraft.blockXWidth
         drawY = ((x - posX) + (y - posY))*Minecraft.blockXWidth/(2*2) + self.height/2 - Minecraft.blockXWidth/(2*2) - (z - posZ)*Minecraft.blockXWidth/2
         self.screen.blit(blockSurface, (drawX, drawY))
         pygame.draw.line(screen, (255, 255, 255), (self.width/2, 0), (self.width/2, self.height))
@@ -334,6 +317,10 @@ class Minecraft(PygameGame):
             self.player.move(0, 0, -1, self.locationMap)
         elif (keyCode == pygame.K_SPACE):
             self.player.placeBlock(BlockObject("stone"), self.locationMap, self.gameBlockGroup)
+        elif (keyCode == pygame.K_1):
+            self.perspective = 1
+        elif (keyCode == pygame.K_2):
+            self.perspective = 2
 
     def keyReleased(self, keyCode, modifier):
         pass
@@ -342,7 +329,7 @@ class Minecraft(PygameGame):
         pass
 
     def redrawAll(self, screen):
-        self.drawWorld(screen, self.player.getPos())
+        self.drawWorld(screen, self.player.getPos(), self.perspective)
     
 game = Minecraft()
         
